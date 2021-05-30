97db5223769ecce04c56ee50c8af13b9f82d915f
Wei Chen <weichen.fiona@gmail.com>
2021-05-30 15:59:25 -0400

    Geojson_linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -29,17 +29,27 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/weiweiVV/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/weiweiVV/Mapping_Earthquakes/main/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data,{ 
+  style: myStyle,
+  onEachFeatures: function (feature,layer){
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "<h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");  
+  }
+})
+.addTo(map);
 });
-
 
 
 
